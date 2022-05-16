ebc54027cdc62d8c115722b485ec0274ef0bcf0f
israelchamberlndie <israelchamberlndie@gmail.com>
2022-05-16 14:39:40 -0400

    new modification

--- sonarqube.sh
+++ sonarqube.sh
@@ -5,12 +5,19 @@
 sudo yum update -y
 sleep 2
 sudo yum install java-11-openjdk-devel -y
+sleep 2
 sudo yum install java-11-openjdk -y
-cd /opt 
-sudo yum install wget -y to install
+sleep 2
+cd /opt
+sleep 2 
+sudo yum install wget -y 
+sleep 2
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sleep 2
 sudo yum install unzip -y
+sleep 2
 sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sleep 2
 cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
 ./sonar.sh start
 # installation done successfully
